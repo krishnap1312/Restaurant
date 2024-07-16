274057406d7aa7f1b62d36452c4372f9000ade96
BH V KRISHNA PRASAD <113228728+krishnap1312@users.noreply.github.com>
2024-07-16 15:09:16 +0530

    Update authenticateUser.js

--- Backend/middleware/authenticateUser.js
+++ Backend/middleware/authenticateUser.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET = process.env.JWT_SECRET || 'your_default_secret';
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const authenticateUser = (req, res, next) => {
     const token = req.headers['authorization'];
